dfe1b6527b2a5b3e9ebb24fc05f4a9f146498b58
rusperres <jairusjaspervc@gmail.com>
2026-01-13 19:28:58 +0800

    VRx and VRy working

--- main/main.c
+++ main/main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include "driver/gpio.h"
+#include "driver/adc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#define LED_PIN 13
+#define SW_PIN 13
+#define VRX_PIN 36
+#define VRY_PIN 39
+
 
 void app_main(void){
-    gpio_reset_pin(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_INPUT);
-    gpio_pullup_en(LED_PIN);
+    adc1_config_width(ADC_WIDTH_BIT_12); // 12-bit ADC (0-4095)
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11); // VRX_PIN = VP/GPIO36
+    adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11); // VRY_PIN = VN/GPIO39
+
+    
+    gpio_reset_pin(SW_PIN);
+    gpio_set_direction(SW_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(SW_PIN);
+    
     while(1){
-        int STATE = gpio_get_level(LED_PIN);
-        printf("%d\n", STATE);
+        int sw = gpio_get_level(SW_PIN);
+        int x = adc1_get_raw(ADC1_CHANNEL_0); // VRX
+        int y = adc1_get_raw(ADC1_CHANNEL_3); // VRY
+
+        printf("X=%d, Y=%d, SW=%d\n", x, y, sw);
         vTaskDelay(1);
     }
 }
